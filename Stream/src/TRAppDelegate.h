044b0c442f049721e648c877421f3cd610c63c0c
Peter Tsoi <petertsoi@gmail.com>
2012-11-27 00:30:59 -0800

    Default XCode setup

---
+++ Stream/src/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  Stream
+//
+//  Created by Peter Tsoi on 11/27/12.
+//  Copyright (c) 2012 TeamRocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
