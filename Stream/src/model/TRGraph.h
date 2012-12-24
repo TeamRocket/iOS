a6849b1e512a5bc1cf9f7920c374971df7ba6ff0
Peter Tsoi <petertsoi@gmail.com>
2012-12-24 14:24:02 -0800

    Automatically log in if user defaults intact

--- Stream/src/model/TRGraph.h
+++ Stream/src/model/TRGraph.h
@@ -34,6 +34,7 @@
 - (void)unregisterForDelegateCallback:(id<TRGraphDelegate>)delegate;
 
 - (void)loginAsUser:(NSString*)first password:(NSString*)password;
+- (void)addUser:(TRUser*)user;
 - (void)signupWithPhone:(NSString*)phone first:(NSString*)first last:(NSString*)last password:(NSString*)password;
 - (TRUser*)getUserWithPhone:(NSString*)phone;
 - (TRPhoto*)getPhotoWithURL:(NSURL*)url;
