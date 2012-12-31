d15de312663ec5ba372417f4d4dea56d2d3c7acb
Peter Tsoi <petertsoi@gmail.com>
2012-12-30 17:22:31 -0800

    Expect null for no latest picture

--- Stream/src/model/TRGraph.m
+++ Stream/src/model/TRGraph.m
@@ -315,7 +315,7 @@ typedef enum {
             newStream.numPhotos = [[streamData objectForKey:@"numberOfPictures"] intValue];
         }
         NSDictionary * latestInfo = [streamData objectForKey:@"latestPicture"];
-        if (![[NSNull null] isEqual:latestInfo] && ![latestInfo respondsToSelector:@selector(removeLastObject)]) {
+        if (![[NSNull null] isEqual:latestInfo]) {
             TRPhoto * latestPhoto = [self getPhotoWithID:[latestInfo objectForKey:@"id"]];
             if (latestPhoto == nil) {
                 latestPhoto = [[TRPhoto alloc] initWithID:[latestInfo objectForKey:@"id"]
