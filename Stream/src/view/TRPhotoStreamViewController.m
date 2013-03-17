7ddd73cea3daae3d315ea987f707f315e721b697
Peter Tsoi <petertsoi@gmail.com>
2013-03-16 17:15:29 -0700

    Remove editing/crop from photo upload

--- Stream/src/view/TRPhotoStreamViewController.m
+++ Stream/src/view/TRPhotoStreamViewController.m
@@ -102,7 +102,7 @@
 - (void)showPhotoPicker {
     if (!mPicker) {
         mPicker = [[UIImagePickerController alloc] init];
-        [mPicker setAllowsEditing:YES];
+        [mPicker setAllowsEditing:NO];
         [mPicker setDelegate:self];
     }
     if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
